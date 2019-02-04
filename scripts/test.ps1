32a48f27009c455a004a1123388de653e3114326
Fabio Ostlind <fabio.ostlind@afconsult.com>
2019-02-04 08:38:45 +0100

    Update test.ps1, migration now works

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -21,8 +21,8 @@ az artifacts universal download --organization "https://722610.visualstudio.com/
 az artifacts universal download --organization "https://722610.visualstudio.com/"  --feed "ArtifactsFeed" --name "flintgroup-daemon-artifact" --version "0.7.0" --path './'
 
 
-  Set-Location C:\MomServices\executor
+  Set-Location C:\MomServices\executor\bin
 
   $ef = "C:\Users\A501205\.nuget\packages\microsoft.entityframeworkcore.tools\2.2.1\tools\netcoreapp2.0\any\ef.dll"
 
-  dotnet exec --depsfile '.\Af.Mom.Services.Executor.Api.deps.json' --root-namespace 'Af.Mom.Services.Executor.Api' --assembly '.\Af.Mom.Services.Executor.Api.dll' --runtimeconfig '.\Af.Mom.Services.Executor.Api.runtimeconfig.json' 'C:\Users\A501205\.nuget\packages\microsoft.entityframeworkcore.tools\2.2.1\tools\netcoreapp2.0\any\ef.dll' database update
+  dotnet exec --depsfile '.\Af.Mom.Services.Executor.Daemon.deps.json' --runtimeconfig '.\Af.Mom.Services.Executor.Daemon.runtimeconfig.json' 'C:\Users\A501205\.nuget\packages\microsoft.entityframeworkcore.tools\2.2.1\tools\netcoreapp2.0\any\ef.dll' database update --assembly '.\Af.Mom.Services.Executor.Daemon.dll' --root-namespace 'Af.Mom.Services.Executor.Deamon' --project-dir '\.' --verbose
